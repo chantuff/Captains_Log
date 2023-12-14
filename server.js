a74ff0777070d6c8e99c5b3de7c16ab1363d8518
chantuff <chan.tuff@yahoo.com>
2023-12-14 11:16:34 -0500

    installed dependencies

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // Import the Express module
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const port = 3000;
+const mongoose = require('mongoose');
 
 // create a newroute in your server.js- be sure to follow the Restful convention
-app.get('/', (req, res) => {
+app.get('/new', (req, res) => {
     res.send('new');
   });
---
+++ views/New.jsx
@@ -0,0 +1,20 @@
+const React = require('react');
+
+class New extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>Captains Log</h1>
+                {/* NOTE: action will be the route, method will be the HTTP verb */}
+                <form action='/logs' method="POST">
+                    Title: <input type="text" title="title" /><br />
+                    Entry: < input type="textarea" name="entry"/> <br />
+                    shipIsBroken: <input type="checkbox" name="shipIsBroken"/> <br />
+                    <input type="submit" name="" value="Submit Entry"/>
+                </form>
+            </div>
+        )
+    }
+}
+
+module.exports = New;
